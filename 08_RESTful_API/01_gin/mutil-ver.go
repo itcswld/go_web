56d23f7e47a8fe064fdb024132d4e5f2eb18ed12
itcswld <itcswld@gmail.com>
2023-11-16 23:36:34 +0800

    restful api

--- 08_RESTful_API/01_gin/mutil-ver.go
+++ 08_RESTful_API/01_gin/mutil-ver.go
@@ -6,15 +6,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// 支援多個API版本
+// 分組路由
 func main() {
 	r := gin.Default()
 	v1 := r.Group("/v1")
 	{
-		v1.GET("/home", controllers.HomeV1Page)
+		v1.GET("/home", controllers.HomeV1Page) //  url = "v1/home"
 	}
 	v2 := r.Group("/v2")
 	{
-		v2.GET("/home", controllers.HomeV2Page)
+		v2.GET("/home", controllers.HomeV2Page) //  url = "v2/home"
 	}
 	r.Run(":80")
 }
